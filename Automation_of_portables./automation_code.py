17f8cf8c08b9fdddd08a2dc6274a60db2b7962c3
shreyajoshi08 <49077392+shreyajoshi08@users.noreply.github.com>
2023-03-09 15:57:28 -0500

    Update automation_code.py

--- Automation_of_portables./automation_code.py
+++ Automation_of_portables./automation_code.py
@@ -30,3 +30,48 @@ def gauge_AdjustmentCAL():
     Unit = wb['AutoCAL']['B4'].value
     ec_WAITmS = 100
     path = wb.path
+
+    wb['AutoCAL']['L3'] = "gauge_Adjustment"
+    
+    if wb['AutoCAL']['D3'].value == "PM620":
+        SN = input("Please enter the serial number of the PM620: ")
+    elif wb['AutoCAL']['D3'].value == "UPM":
+        SN = input("Please enter the serial number of the UPM: ")
+    elif wb['AutoCAL']['D3'].value == "PACE1000":
+        SN = input("Please choose range of the PACE1000 (1 or 2 or 3): ")
+        wb['AutoCAL']['S7'] = Pace1000sensor
+    elif wb['AutoCAL']['D3'].value == "DPI610":
+        rc = input("Is it external sensor? (Y/N)")
+        if rc == 'Y':
+            wb['AutoCAL']['M1'] = 1
+    elif wb['AutoCAL']['M1'].value == 1:
+        SN = input("Please enter the serial number of external sensor: ")
+        wb['AutoCAL']['S7'] = SN
+    elif wb['AutoCAL']['D3'].value == "DPI705E":
+        rc = input("Are there Negative pressure option? (Y/N)")
+        if rc == 'Y':
+            option705E = 0
+        else:
+            option705E = 1
+    elif wb['AutoCAL']['D3'].value == "DPI800":
+        rc = input("Which is range of DPI800? If P1, please select Y. P2 is N.")
+        if rc == 'Y':
+            range_800 = 2
+        else:
+            range_800 = 3
+    
+    rc = input("Do you perform pre-check? (Y/N)")
+    if rc == 'Y':
+        precheck = 0
+    else:
+        precheck = 1
+    
+    if wb['AutoCAL']['E3'].value == "CM":
+        CMlimitcheckg()
+    
+    rc = input("Have you checked Limit and FS? (Y/N)")
+    if rc == 'Y':
+        print("Start process.")
+    else:
+        print("Stop process.")
+        return
